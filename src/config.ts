969bc960705016c9e317b4665e0c8164cbaf1bfa
John McMahon <jmcmahon@radiantblue.com>
2017-08-18 15:25:16 -0400

    Extract urls

--- src/config.ts
+++ src/config.ts
@@ -17,6 +17,8 @@
 export const CLASSIFICATION_BANNER_BACKGROUND = process.env.CLASSIFICATION_BANNER_BACKGROUND
 export const CLASSIFICATION_BANNER_FOREGROUND = process.env.CLASSIFICATION_BANNER_FOREGROUND
 export const CLASSIFICATION_BANNER_TEXT       = process.env.CLASSIFICATION_BANNER_TEXT
+export const OSM_BASE_URL                     = process.env.OSM_BASE_URL
+export const PLANET_BASE_URL                  = process.env.PLANET_BASE_URL
 
 export const API_ROOT = process.env.API_ROOT
 export const CONSENT_BANNER_TEXT = {__html: process.env.CONSENT_BANNER_TEXT}
@@ -34,7 +36,7 @@ export const SESSION_IDLE_STORE = 'lastActivity'
 export const BASEMAP_TILE_PROVIDERS = [
   {
     name: 'OSM',
-    url: 'https://osm.geointservices.io/osm_tiles/{z}/{x}/{y}.png64',
+    url: `https://${OSM_BASE_URL}/osm_tiles/{z}/{x}/{y}.png64`,
     attributions: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>',
     maxZoom: 20,
   },
@@ -43,19 +45,19 @@ export const BASEMAP_TILE_PROVIDERS = [
 export const SCENE_TILE_PROVIDERS = [
   {
     prefix: 'planetscope',
-    url: 'https://tiles{0-3}.planet.com/v1/experimental/tiles/PSOrthoTile/__SCENE_ID__/{z}/{x}/{y}.png?api_key=__API_KEY__',
+    url: `https://tiles{0-3}.${PLANET_BASE_URL}/v1/experimental/tiles/PSOrthoTile/__SCENE_ID__/{z}/{x}/{y}.png?api_key=__API_KEY__`,
     maxZoom:  13,
     attributions: '&copy; <a href="https://www.planet.com" target="_blank" rel="noopener">Planet Labs (PlanetScope)</a>',
   },
   {
     prefix: 'rapideye',
-    url: 'https://tiles{0-3}.planet.com/v1/experimental/tiles/REOrthoTile/__SCENE_ID__/{z}/{x}/{y}.png?api_key=__API_KEY__',
+    url: `https://tiles{0-3}.${PLANET_BASE_URL}/v1/experimental/tiles/REOrthoTile/__SCENE_ID__/{z}/{x}/{y}.png?api_key=__API_KEY__`,
     maxZoom:  13,
     attributions: '&copy; <a href="https://www.planet.com" target="_blank" rel="noopener">Planet Labs (RapidEye)</a>',
   },
   {
     prefix: 'landsat',
-    url: 'https://tiles{0-3}.planet.com/v1/experimental/tiles/Landsat8L1G/__SCENE_ID__/{z}/{x}/{y}.png?api_key=__API_KEY__',
+    url: `https://tiles{0-3}.${PLANET_BASE_URL}/v1/experimental/tiles/Landsat8L1G/__SCENE_ID__/{z}/{x}/{y}.png?api_key=__API_KEY__`,
     maxZoom:  13,
     attributions: '&copy; <a href="https://www.planet.com" target="_blank" rel="noopener">Planet Labs (Landsat8)</a>',
   },
