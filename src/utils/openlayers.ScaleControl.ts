8e6ff41c29ed5a0c6efdda7bc92772aaa7895bd5
Greg Barrett <gbarrett@radiantblue.com>
2017-11-13 14:13:50 -0500

    [#12528] Make the scale indicator editable.

--- src/utils/openlayers.ScaleControl.ts
+++ src/utils/openlayers.ScaleControl.ts
@@ -21,37 +21,98 @@ const DPI = 100
 const MULTIPLIER = DPI / 0.0254
 
 export class ScaleControl extends ol.control.Control {
+  $value: any
+
   constructor(className: string) {
-    const element = document.createElement('div')
+    const element: any = document.createElement('div')
     super({ element })
 
     element.className = [className, 'ol-unselectable', 'ol-control'].filter(s => s).join(' ')
-    element.title = 'Map Scale'
-    element.innerHTML = '<div class="value"></div>'
+    element.title = 'Map Scale (click to edit)'
+    element.innerHTML = `<div>
+      1&#8197;:&#8197;<span
+        class="value"
+        contenteditable="true"
+      ></span>
+    </div>`
+
+    this.$value = element.querySelector('.value') as any
+    this.$value.onblur = this.blur.bind(this)
+    this.$value.onkeydown = this.keydown.bind(this)
 
     setTimeout(() => {
       this.getMap().getView().on('change:resolution', event => {
         let res = event.target.getResolution()
-        let scale = toSignificantDigits(toScale(res), 3)
+        let scale = toSignificantDigits(toScale(res))
 
-        element.querySelector('.value').innerHTML = `<strong>${scale}</strong>&#8197;:&#8197;1`
-        element.title = `Scale: ${scale} : 1`
+        this.$value.innerText = scale
       }, this)
     })
   }
+
+  private blur() {
+    const scale = +toUnformattedNumber(this.$value.innerText)
+    const map = this.getMap()
+    const view: any = map.getView()
+    const oldResolution = view.getResolution()
+    let resolution = toResolution(scale)
+
+    resolution = resolution < view.getMaxResolution() ? resolution : view.getMaxResolution()
+    resolution = resolution > view.getMinResolution() ? resolution : view.getMinResolution()
+
+    map.beforeRender(ol.animation.zoom({
+      duration: 1618 * Math.abs(Math.log10(resolution / oldResolution)),
+      resolution: oldResolution,
+    }))
+
+    view.setResolution(resolution)
+  }
+
+  private keydown(event) {
+    event.stopPropagation()
+
+    switch (event.key) {
+      case ',':
+      case '0':
+      case '1':
+      case '2':
+      case '3':
+      case '4':
+      case '5':
+      case '6':
+      case '7':
+      case '8':
+      case '9':
+      case 'ArrowDown':
+      case 'ArrowLeft':
+      case 'ArrowRight':
+      case 'ArrowUp':
+      case 'Backspace':
+      case 'Delete':
+      case 'Tab':
+        break
+      case 'Enter':
+        this.$value.blur()
+        break
+      default:
+        event.preventDefault()
+    }
+  }
 }
 
-function toSignificantDigits(n: number, s: number = 4): string {
+function toUnformattedNumber(n) {
+  return +n.toString().replace(/[\s,]+|\..*/g, '')
+}
+
+function toSignificantDigits(n: number, s: number = 3): string {
   let m = Math.pow(10, Math.ceil(Math.log10(n)) - s)
 
   return (m * Math.round(n / m)).toLocaleString()
 }
 
-/*
 function toResolution(scale: number): number {
   return scale / MULTIPLIER
 }
-*/
 
 function toScale(resolution: number): number {
   return MULTIPLIER * resolution
