fdfc55fbcca507b32f8a60f7ef2ffa9f5e8de82a
John McMahon <jmcmahon@radiantblue.com>
2018-05-16 11:30:40 -0400

    Working on encoding

--- src/components/PrimaryMap.tsx
+++ src/components/PrimaryMap.tsx
@@ -1236,6 +1236,147 @@ function tileLoadFunction(imageTile, src) {
   }
 }
 
+/**
+ *
+ *  Base64 encode / decode
+ *  http://www.webtoolkit.info
+ *
+ **/
+const Base64 = {
+
+  // Private property
+  _keyStr: 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/='
+
+  // Public method for encoding
+  , encode: function (input)
+  {
+    let output = ''
+    let chr1, chr2, chr3, enc1, enc2, enc3, enc4
+    let i = 0
+
+    input = Base64._utf8_encode(input)
+
+    while (i < input.length) {
+      chr1 = input.charCodeAt(i++)
+      chr2 = input.charCodeAt(i++)
+      chr3 = input.charCodeAt(i++)
+
+      enc1 = chr1 >> 2
+      enc2 = ((chr1 & 3) << 4) | (chr2 >> 4)
+      enc3 = ((chr2 & 15) << 2) | (chr3 >> 6)
+      enc4 = chr3 & 63
+
+      if (isNaN(chr2)) {
+        enc3 = enc4 = 64
+      }
+      else if (isNaN(chr3)) {
+        enc4 = 64
+      }
+
+      output = output +
+        this._keyStr.charAt(enc1) + this._keyStr.charAt(enc2) +
+        this._keyStr.charAt(enc3) + this._keyStr.charAt(enc4)
+    } // Whend
+
+    return output
+  } // End Function encode
+
+  // Public method for decoding
+  , decode: function (input)
+  {
+    let output = ''
+    let chr1, chr2, chr3
+    let enc1, enc2, enc3, enc4
+    let i = 0
+
+    input = input.replace(/[^A-Za-z0-9\+\/\=]/g, '')
+    while (i < input.length) {
+      enc1 = this._keyStr.indexOf(input.charAt(i++))
+      enc2 = this._keyStr.indexOf(input.charAt(i++))
+      enc3 = this._keyStr.indexOf(input.charAt(i++))
+      enc4 = this._keyStr.indexOf(input.charAt(i++))
+
+      chr1 = (enc1 << 2) | (enc2 >> 4)
+      chr2 = ((enc2 & 15) << 4) | (enc3 >> 2)
+      chr3 = ((enc3 & 3) << 6) | enc4
+
+      output = output + String.fromCharCode(chr1)
+
+      if (enc3 !== 64) {
+        output = output + String.fromCharCode(chr2)
+      }
+
+      if (enc4 !== 64) {
+        output = output + String.fromCharCode(chr3)
+      }
+
+    } // Whend
+
+    output = Base64._utf8_decode(output)
+
+    return output
+  } // End Function decode
+
+  // Private method for UTF-8 encoding
+  , _utf8_encode: function (str)
+  {
+    let utftext = ''
+    str = str.replace(/\r\n/g, '\n')
+
+    for (let n = 0; n < str.length; n++) {
+      let c = str.charCodeAt(n)
+
+      if (c < 128) {
+        utftext += String.fromCharCode(c)
+      }
+      else if ((c > 127) && (c < 2048)) {
+        utftext += String.fromCharCode((c >> 6) | 192)
+        utftext += String.fromCharCode((c & 63) | 128)
+      }
+      else {
+        utftext += String.fromCharCode((c >> 12) | 224)
+        utftext += String.fromCharCode(((c >> 6) & 63) | 128)
+        utftext += String.fromCharCode((c & 63) | 128)
+      }
+
+    } // Next n
+
+    return utftext
+  } // End Function _utf8_encode
+
+  // Private method for UTF-8 decoding
+  , _utf8_decode: function (utftext) {
+    let str = ''
+    let i = 0
+    let c, c1, c2, c3
+    c = c1 = c2 = 0
+
+    while (i < utftext.length) {
+      c = utftext.charCodeAt(i)
+
+      if (c < 128) {
+        str += String.fromCharCode(c)
+        i++
+      }
+      else if ((c > 191) && (c < 224)) {
+        c2 = utftext.charCodeAt(i + 1)
+        str += String.fromCharCode(((c & 31) << 6) | (c2 & 63))
+        i += 2
+      }
+      else {
+        c2 = utftext.charCodeAt(i + 1)
+        c3 = utftext.charCodeAt(i + 2)
+        str += String.fromCharCode(((c & 15) << 12) | ((c2 & 63) << 6) | (c3 & 63))
+        i += 3
+      }
+
+    } // Whend
+
+    return str
+  }, // End Function _utf8_decode
+
+}
+
 function detectionTileLoadFunction(imageTile, src) {
   if (imageTile.loadingError) {
     delete imageTile.loadingError
@@ -1248,21 +1389,21 @@ function detectionTileLoadFunction(imageTile, src) {
     const apiKey = getCookie('api_key')
     const auth = 'Basic ' + btoa(apiKey + ':')
     client.setRequestHeader('Authorization', auth)
-    // client.responseType = 'arraybuffer'
+    // Client.responseType = 'arraybuffer'
     client.onload = function() {
-      const data = 'data:image/png;base64,' + b64EncodeUnicode(client.response)
+      const data = 'data:image/png;base64,' + Base64.encode(client.response)
       imageTile.getImage().src = data
     }
     client.send()
   }
 }
 
-function b64EncodeUnicode(str) {
+/*function b64EncodeUnicode(str) {
   return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
     console.log(match)
     return String.fromCharCode(parseInt(p1, 16))
   }))
-}
+}*/
 
 function getCookie(name) {
   const match = document.cookie.match(new RegExp(name + '=([^;]+)'))
