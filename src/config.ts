fd517d33867d3adf0dd3046d769c900a790dfd46
Filip Sufitchi <filips@imagemattersllc.com>
2018-06-18 11:54:45 -0400

    Revert to only using maxZoom for local index images

--- src/config.ts
+++ src/config.ts
@@ -87,8 +87,7 @@ export const SCENE_TILE_PROVIDERS = [
   },
   {
     attributions: '<a href="https://docs.opendata.aws/landsat-pds/readme.html">Landsat PDS</a>',
-    minZoom:  11,
-    maxZoom:  11,
+    maxZoom:  13,
     name: 'Landsat8',
     prefix: 'landsat_pds',
     catalogSection: 'localindex', // Look in /localindex/* in the catalog
