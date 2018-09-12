3893d7f5b602f7ab5551519c5add050c6a6b699c
Patrick D <go0ty@users.noreply.github.com>
2018-09-12 10:35:49 -0400

    Fix Frame Style Updates (#235)
    * Fix Frame Style Updates
    
    * Consolidated Update Styles

--- src/components/PrimaryMap.tsx
+++ src/components/PrimaryMap.tsx
@@ -234,7 +234,6 @@ export class PrimaryMap extends React.Component<Props, State> {
     if (previousProps.selectedFeature !== this.props.selectedFeature) {
       this.renderSelectionPreview()
       this.updateSelectedFeature()
-      this.updateStyles()
     }
 
     if (previousProps.detections !== this.props.detections) {
@@ -264,7 +263,6 @@ export class PrimaryMap extends React.Component<Props, State> {
     if (previousProps.frames !== this.props.frames) {
       this.renderFrames()
       this.renderPins()
-      this.updateStyles()
     }
 
     if (previousProps.imagery !== this.props.imagery || routeChanged) {
@@ -290,6 +288,13 @@ export class PrimaryMap extends React.Component<Props, State> {
 
     if (previousProps.view !== this.props.view && this.props.view) {
       this.updateView()
+    }
+
+    if ((!previousProps.view) || 
+      (previousProps.view.zoom !== this.props.view.zoom && this.props.view) ||
+      (previousProps.selectedFeature !== this.props.selectedFeature) ||
+      (previousProps.frames !== this.props.frames)) {
+      this.updateStyles();
     }
 
     if ((previousProps.mode !== this.props.mode) ||
