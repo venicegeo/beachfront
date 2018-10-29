bfaca12e7d5f87e73fce1dacb8977e1dc37fb9b0
Filip Sufitchi <filips@imagemattersllc.com>
2018-10-29 14:56:14 -0400

    Use underscores for imagery sources for consistency

--- src/config.ts
+++ src/config.ts
@@ -53,7 +53,7 @@ export const SCENE_TILE_PROVIDERS = [
     attributions: '&copy; <a href="https://www.planet.com" target="_blank" rel="noopener">Planet Labs (Copernicus Sentinel-2)</a>',
     maxZoom:  13,
     name: 'Copernicus Sentinel-2',
-    prefix: 'sentinel-planet',
+    prefix: 'sentinel_planet',
     catalogSection: 'planet', // Look in /planet/* in the catalog
     provider: 'Planet',
     url: `https://tiles{0-3}.${PLANET_BASE_URL}/v1/experimental/tiles/Sentinel2L1C/__SCENE_ID__/{z}/{x}/{y}.png?api_key=__API_KEY__`,
@@ -64,7 +64,7 @@ export const SCENE_TILE_PROVIDERS = [
     attributions: '&copy; <a href="https://www.planet.com" target="_blank" rel="noopener">Planet Labs (Copernicus Sentinel-2)</a>',
     maxZoom: 13,
     name: 'Copernicus Sentinel-2',
-    prefix: 'sentinel-s3',
+    prefix: 'sentinel_s3',
     catalogSection: 'planet', // Look in /planet/* in the catalog
     provider: 'Planet with S3 imagery',
     url: `https://tiles{0-3}.${PLANET_BASE_URL}/v1/experimental/tiles/Sentinel2L1C/__SCENE_ID__/{z}/{x}/{y}.png?api_key=__API_KEY__`,
