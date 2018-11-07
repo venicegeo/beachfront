3afd368be7edc2683ce548425e4974d6609b22a3
Lars Kendall <lars@prominentedge.com>
2018-11-07 15:44:14 -0800

    User reducer tests.

---
+++ test/reducers/userReducer.test.tsx
@@ -0,0 +1,94 @@
+/*
+ * Copyright 2018, RadiantBlue Technologies, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+import {userInitialState, userReducer} from '../../src/reducers/userReducer'
+import {types} from '../../src/actions/userActions'
+
+describe('userReducer', () => {
+  it('initialState', () => {
+    expect(userReducer(undefined, {})).toEqual(userInitialState)
+  })
+
+  it('USER_DESERIALIZED', () => {
+    const action = {
+      type: types.USER_DESERIALIZED,
+      deserialized: {
+        some: 'data',
+      },
+    }
+
+    expect(userReducer(userInitialState, action)).toEqual({
+      ...userInitialState,
+      ...action.deserialized,
+    })
+  })
+
+  it('USER_LOGGED_OUT', () => {
+    const state = {
+      ...userInitialState,
+      isLoggedIn: true,
+    }
+
+    const action = { type: types.USER_LOGGED_OUT }
+
+    expect(userReducer(state, action)).toEqual({
+      ...state,
+      isLoggedIn: false,
+      isSessionLoggedOut: true,
+    })
+  })
+
+  it('USER_SESSION_CLEARED', () => {
+    const state = {
+      ...userInitialState,
+      isLoggedIn: true,
+      isSessionExpired: true,
+    }
+
+    const action = { type: types.USER_SESSION_CLEARED }
+
+    expect(userReducer(state, action)).toEqual({
+      ...state,
+      isLoggedIn: false,
+      isSessionExpired: false,
+    })
+  })
+
+  it('USER_SESSION_LOGGED_OUT', () => {
+    const state = {
+      ...userInitialState,
+      isLoggedIn: true,
+      isSessionLoggedOut: true,
+    }
+
+    const action = { type: types.USER_SESSION_LOGGED_OUT }
+
+    expect(userReducer(state, action)).toEqual({
+      ...state,
+      isLoggedIn: false,
+      isSessionLoggedOut: false,
+    })
+  })
+
+  it('USER_SESSION_EXPIRED', () => {
+    const action = { type: types.USER_SESSION_EXPIRED }
+
+    expect(userReducer(userInitialState, action)).toEqual({
+      ...userInitialState,
+      isSessionExpired: true,
+    })
+  })
+})
