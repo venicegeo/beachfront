18d51fe694d5f9524f2f0ea74bab2f82298a8bfb
Lars Kendall <lars@prominentedge.com>
2018-11-13 13:00:58 -0800

    Tweak api status action test name.

--- test/actions/apiStatusActions.test.ts
+++ test/actions/apiStatusActions.test.ts
@@ -58,7 +58,7 @@ describe('apiStatusActions', () => {
     ])
   })
 
-  it('fetch (error)', async () => {
+  it('fetch (request error)', async () => {
     mockAdapter.onGet('/').reply(400, 'error')
 
     await store.dispatch(apiStatusActions.fetch())
